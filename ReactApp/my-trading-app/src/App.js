9801decd29b253296017d702dc6ffa208d36666c
subbu <subbutrademap@gmail.com>
2025-07-02 14:26:25 -0400

    Day4 class added

--- ReactApp/my-trading-app/src/App.js
+++ ReactApp/my-trading-app/src/App.js
@@ -3,12 +3,16 @@ import Login from './Day3/Login';
 import Welcome from './Day3/Welcome';
 import GridLayout from './Day3/GridLayout';
 import Home from './Day3/Home';
+import Calculator from './Day4/Calculator';
+import LoginForm from './Day4/LoginForm';
 
 const App = () => (
   <>
     {/* <Welcome /> */}
     {/* <GridLayout /> */}
-    <Home />
+    {/* <Home /> */}
+    {/* <Calculator /> */}
+    <LoginForm />
   </>
 );
 
